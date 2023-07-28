4984c0fb99044fccecf060b4e21de6c369ec6fac
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-28 21:30:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const byteSize = (str) => {
   // write your code here
+	// empty string--> size 0 
+	let count = 0
+	if(str == ''){
+		return 0
+	}
+	else{
+		let arr = str.split('');
+		count = arr.length;
+		// for(let i=0; i<n ; i++) {
+		// 	count++
+		// }
+		// return count
+		
+	}
+	
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
